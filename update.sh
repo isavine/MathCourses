6c74dc21e3bdd9104269f0be2008e7aa82320f1e
Igor Savine <isavine@gmail.com>
2020-03-29 22:56:38 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ python get_schedule.py -t 2202 -o spring-sections-$d.json
 python write_csv.py -t 2202 -o spring-classes-$d.csv
 python get_schedule.py -t 2205 -o summer-sections-$d.json
 python write_csv.py -t 2205 -o summer-classes-$d.csv
-#python get_schedule.py -t 2208 -o fall-sections-$d.json
-#python write_csv.py -t 2208 -o fall-classes-$d.csv
+python get_schedule.py -t 2208 -o fall-sections-$d.json
+python write_csv.py -t 2208 -o fall-classes-$d.csv
 find . -name \*-[0-3][0-9].\* -mtime +3 | xargs -r rm

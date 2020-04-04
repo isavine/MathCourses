1abe1bc73f359970a2b39450d603049b3abb6e07
Igor Savine <igor@math.berkeley.edu>
2020-04-04 08:03:33 -0400

    add maxdepth option to find

--- update.sh
+++ update.sh
@@ -8,4 +8,4 @@ python get_schedule.py -t 2205 -o summer-sections-$d.json
 python write_csv.py -t 2205 -o summer-classes-$d.csv
 python get_schedule.py -t 2208 -o fall-sections-$d.json
 python write_csv.py -t 2208 -o fall-classes-$d.csv
-find . -name \*-[0-3][0-9].\* -mtime +3 | xargs -r rm
+find . -maxdepth 1 -name \*-[0-3][0-9].\* -mtime +3 | xargs -r rm

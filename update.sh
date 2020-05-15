53e81f7d6b5c3e33971147b02bc6a93f1bb31eac
Igor Savine <igor@math.berkeley.edu>
2020-05-15 07:43:20 -0400

    commented out spring classes

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 cd `dirname $0`
 [ -r venv/bin/activate ] && . venv/bin/activate
 d=`date '+%d'`
-python get_classes.py -t 2202 -o spring-classes-$d.json
-python write_csv.py -t 2202 -o spring-classes-$d.csv
+#python get_classes.py -t 2202 -o spring-classes-$d.json
+#python write_csv.py -t 2202 -o spring-classes-$d.csv
 python get_classes.py -t 2205 -o summer-classes-$d.json
 python write_csv.py -t 2205 -o summer-classes-$d.csv
 python get_classes.py -t 2208 -o fall-classes-$d.json

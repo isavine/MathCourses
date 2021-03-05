d9f83cf0590b63412cf2121471afe506cde27c1f
Igor Savine <igor@math.berkeley.edu>
2021-03-05 12:19:58 -0800

    adjusted spring/summer terms for 2021

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ cd `dirname $0`
 d=`date '+%d'`
 python get_classes.py -t 2212 -o spring-classes-$d.json
 python write_csv.py -t 2212 -o spring-classes-$d.csv
-#python get_classes.py -t 2205 -o summer-classes-$d.json
-#python write_csv.py -t 2205 -o summer-classes-$d.csv
-python get_classes.py -t 2208 -o fall-classes-$d.json
-python write_csv.py -t 2208 -o fall-classes-$d.csv
+python get_classes.py -t 2205 -o summer-classes-$d.json
+python write_csv.py -t 2205 -o summer-classes-$d.csv
+#python get_classes.py -t 2208 -o fall-classes-$d.json
+#python write_csv.py -t 2208 -o fall-classes-$d.csv
 find . -maxdepth 1 -name \*-[0-3][0-9].\* -mtime +3 | xargs -r rm

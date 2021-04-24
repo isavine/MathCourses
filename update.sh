31b7932ce853a3e3720a6e2ea57d35cffdbb55a2
Igor Savine <isavine@gmail.com>
2021-04-24 04:24:31 -0700

    added fall 2021 semester

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 cd `dirname $0`
 [ -r venv/bin/activate ] && . venv/bin/activate
 d=`date '+%d'`
-python get_classes.py -t 2212 -o spring-classes-$d.json
-python write_csv.py -t 2212 -o spring-classes-$d.csv
+#python get_classes.py -t 2212 -o spring-classes-$d.json
+#python write_csv.py -t 2212 -o spring-classes-$d.csv
 python get_classes.py -t 2215 -o summer-classes-$d.json
 python write_csv.py -t 2215 -o summer-classes-$d.csv
-#python get_classes.py -t 2218 -o fall-classes-$d.json
-#python write_csv.py -t 2218 -o fall-classes-$d.csv
+python get_classes.py -t 2218 -o fall-classes-$d.json
+python write_csv.py -t 2218 -o fall-classes-$d.csv
 find . -maxdepth 1 -name \*-[0-3][0-9].\* -mtime +3 | xargs -r rm

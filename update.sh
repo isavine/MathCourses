e1bf11342415fb3a2aec2c8c5a914b6644ff7e9d
Igor Savine <isavine@gmail.com>
2025-04-08 07:39:49 -0700

    update semester numbers

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 cd `dirname $0`
 [ -r venv/bin/activate ] && . venv/bin/activate
 d=`date '+%d'`
-python get_classes.py -t 2242 -o spring-classes-$d.json
-python write_csv.py -t 2242 -o spring-classes-$d.csv
-python get_classes.py -t 2245 -o summer-classes-$d.json
-python write_csv.py -t 2245 -o summer-classes-$d.csv
-python get_classes.py -t 2248 -o fall-classes-$d.json
-python write_csv.py -t 2248 -o fall-classes-$d.csv
+python get_classes.py -t 2252 -o spring-classes-$d.json
+python write_csv.py -t 2252 -o spring-classes-$d.csv
+python get_classes.py -t 2255 -o summer-classes-$d.json
+python write_csv.py -t 2255 -o summer-classes-$d.csv
+python get_classes.py -t 2258 -o fall-classes-$d.json
+python write_csv.py -t 2258 -o fall-classes-$d.csv
 find . -maxdepth 1 -name \*-[0-3][0-9].\* -mtime +30 | xargs -r rm
